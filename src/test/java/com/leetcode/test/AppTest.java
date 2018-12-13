f7bf9845c4fcb2c535573d9d9045d1b0f12c1d13
dengxin.chen@56qq.com <dengxin.chen@56qq.com>
2018-12-13 14:11:02 +0800

    feat():init project

---
+++ src/test/java/com/leetcode/test/AppTest.java
@@ -0,0 +1,17 @@
+package com.leetcode.test;
+
+import org.junit.Test;
+
+/**
+ * @author: dengxin.chen
+ * @date: 2018/12/13 14:07
+ * @description:
+ */
+public class AppTest {
+
+    @Test
+    public void leetCodeTest1()
+    {
+        System.out.println("init project");
+    }
+}
