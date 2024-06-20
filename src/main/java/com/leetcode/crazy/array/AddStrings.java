2abeafde183336aed640e3db79317bf52697d10d
itcrazy0717 <itcrazy0717@gmail.com>
2024-06-20 22:25:28 +0800

    文件格式处理

--- src/main/java/com/leetcode/crazy/array/AddStrings.java
+++ src/main/java/com/leetcode/crazy/array/AddStrings.java
@@ -8,14 +8,14 @@ package com.leetcode.crazy.array;
 public class AddStrings {
 
 	/**
-	 * 给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和。
+	 * 给定两个字符串形式的非负整数num1和num2，计算它们的和。
 	 * <p>
 	 * 注意：
 	 * <p>
-	 * num1 和num2 的长度都小于 5100.
-	 * num1 和num2 都只包含数字 0-9.
+	 * num1 和num2的长度都小于 5100.
+	 * num1 和num2 都只包含数字0-9.
 	 * num1 和num2 都不包含任何前导零。
-	 * 你不能使用任何內建 BigInteger 库， 也不能直接将输入的字符串转换为整数形式。
+	 * 你不能使用任何內建 BigInteger 库，也不能直接将输入的字符串转换为整数形式。
 	 * https://leetcode-cn.com/problems/add-strings/
 	 */
 
